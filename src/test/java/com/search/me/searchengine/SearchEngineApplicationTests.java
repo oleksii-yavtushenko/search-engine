ca32974d3d4ac8e63368a312002c26d9bf01618e
volvozachto <fogmrfog@gmail.com>
2023-02-25 23:01:56 +0100

    initial commit

---
+++ src/test/java/com/search/me/searchengine/SearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.me.searchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
