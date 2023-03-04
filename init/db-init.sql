e7d2ae43cc8373672f5a72e8b243068d26fccf75
volvozachto <fogmrfog@gmail.com>
2023-03-04 19:14:56 +0100

    add db table init

--- init/db-init.sql
+++ init/db-init.sql
@@ -1,3 +1,30 @@
 CREATE ROLE postgres WITH LOGIN ENCRYPTED PASSWORD '';
 
---CREATE TABLE:
+CREATE TABLE Pages (
+                       ID SERIAL PRIMARY KEY,
+                       URL TEXT NOT NULL,
+                       Title TEXT NOT NULL,
+                       Content TEXT NOT NULL
+);
+
+CREATE TABLE Keywords (
+                          ID SERIAL PRIMARY KEY,
+                          Keyword TEXT NOT NULL
+);
+
+CREATE TABLE PageKeywords (
+                              PageID INTEGER REFERENCES Pages(ID),
+                              KeywordID INTEGER REFERENCES Keywords(ID),
+                              PRIMARY KEY (PageID, KeywordID)
+);
+
+CREATE TABLE PageRank (
+                          PageID INTEGER REFERENCES Pages(ID) PRIMARY KEY,
+                          Rank FLOAT NOT NULL
+);
+
+CREATE TABLE SearchHistory (
+                               ID SERIAL PRIMARY KEY,
+                               SearchQuery TEXT NOT NULL,
+                               SearchTime TIMESTAMP NOT NULL DEFAULT NOW()
+);
